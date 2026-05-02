4805f2898da5b1fe178328390830c262a11ee714
Anatol Belski <anbelski@linux.microsoft.com>
2026-05-03 00:00:35 +0200

    tests: V14 vsock SHUTDOWN with undefined flag bits
    Submit a SHUTDOWN packet with undefined flag bits set (beyond
    RCV=1 and SEND=2). The device should ignore undefined bits rather
    than misinterpreting them as valid shutdown directives.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

---
+++ tests/vsock/v0014_shutdown_bad_flags.c
@@ -0,0 +1,61 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * V0014: vsock_shutdown_bad_flags
+ *
+ * Submit a SHUTDOWN packet with undefined flag bits set (bits other than
+ * VIRTIO_VSOCK_SHUTDOWN_RCV=1 and VIRTIO_VSOCK_SHUTDOWN_SEND=2).
+ * The device should ignore undefined bits rather than misinterpreting them.
+ */
+#include "tests/test.h"
+#include "lib/util.h"
+#include "lib/vring.h"
+#include "lib/virtio_pci.h"
+
+#include <string.h>
+#include <unistd.h>
+
+struct virtio_vsock_hdr {
+    uint64_t src_cid;
+    uint64_t dst_cid;
+    uint32_t src_port;
+    uint32_t dst_port;
+    uint32_t len;
+    uint16_t type;
+    uint16_t op;
+    uint32_t flags;
+    uint32_t buf_alloc;
+    uint32_t fwd_cnt;
+} __attribute__((packed));
+
+#define VIRTIO_VSOCK_TYPE_STREAM  1
+#define VIRTIO_VSOCK_OP_SHUTDOWN  4
+
+static test_result_t test_vsock_shutdown_bad_flags(struct virtio_dev *dev,
+                                                   struct vring *vr)
+{
+    struct virtio_vsock_hdr *pkt = vv_alloc_pages(1);
+
+    pkt->src_cid = 3;
+    pkt->dst_cid = 2;
+    pkt->src_port = 1234;
+    pkt->dst_port = 5678;
+    pkt->len = 0;
+    pkt->type = VIRTIO_VSOCK_TYPE_STREAM;
+    pkt->op = VIRTIO_VSOCK_OP_SHUTDOWN;
+    pkt->flags = 0xFFFFFFFC; /* All bits except RCV and SEND set */
+    pkt->buf_alloc = 4096;
+    pkt->fwd_cnt = 0;
+
+    uint64_t pkt_phys = vv_virt_to_phys(pkt);
+
+    vring_raw_set_desc(vr, 0, pkt_phys, sizeof(*pkt), 0, 0);
+
+    vring_raw_set_avail(vr, 0, 0);
+    vring_raw_set_avail_idx(vr, 1);
+
+    return vv_kick_and_wait(dev, vr, 0, VV_TIMEOUT_MS);
+}
+
+REGISTER_TEST(V0014, VIRTIO_PCI_DEVICE_VSOCK, test_vsock_shutdown_bad_flags,
+              "SHUTDOWN with undefined flag bits set",
+              VIRTIO_SPEC_V1_2, "5.10.6.6");
