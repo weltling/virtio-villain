a849a6bbd8417cecb5c8b8daf44546faa4b5bf53
Anatol Belski <anbelski@linux.microsoft.com>
2026-05-04 21:34:24 +0200

    tests: R0003 virtio-mem unplug request
    Submit VIRTIO_MEM_REQ_UNPLUG to unplug a memory block
    per spec 5.14.6.2.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

---
+++ tests/mem/r0003_unplug.c
@@ -0,0 +1,63 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * R0003: Virtio-mem unplug request.
+ *
+ * Submit a VIRTIO_MEM_REQ_UNPLUG request to unplug a memory block.
+ *
+ * Spec 5.14.6.2: The driver unplugs blocks to return memory.
+ */
+#include "tests/test.h"
+#include "lib/util.h"
+#include "lib/vring.h"
+#include "lib/virtio_pci.h"
+
+#include <string.h>
+#include <unistd.h>
+
+struct virtio_mem_req {
+    uint16_t type;
+    uint16_t padding[3];
+    uint64_t addr;
+    uint16_t nb_blocks;
+    uint16_t padding1[3];
+} __attribute__((packed));
+
+struct virtio_mem_resp {
+    uint16_t type;
+    uint16_t padding[3];
+    uint16_t state;
+} __attribute__((packed));
+
+#define VIRTIO_MEM_REQ_UNPLUG 1
+
+static test_result_t test_mem_unplug(struct virtio_dev *dev,
+                                     struct vring *vr)
+{
+    struct virtio_mem_req *req = vv_alloc_pages(1);
+    struct virtio_mem_resp *resp = vv_alloc_pages(1);
+
+    memset(req, 0, sizeof(*req));
+    req->type = VIRTIO_MEM_REQ_UNPLUG;
+    uint64_t region_addr = *(volatile uint64_t *)((uint8_t *)dev->device_cfg + 16);
+    req->addr = region_addr;
+    req->nb_blocks = 1;
+
+    memset(resp, 0xFF, sizeof(*resp));
+
+    uint64_t req_phys = vv_virt_to_phys(req);
+    uint64_t resp_phys = vv_virt_to_phys(resp);
+
+    vring_raw_set_desc(vr, 0, req_phys, sizeof(*req),
+                       VRING_DESC_F_NEXT, 1);
+    vring_raw_set_desc(vr, 1, resp_phys, sizeof(*resp),
+                       VRING_DESC_F_WRITE, 0);
+
+    vring_raw_set_avail(vr, 0, 0);
+    vring_raw_set_avail_idx(vr, 1);
+
+    return vv_kick_and_wait(dev, vr, 0, VV_TIMEOUT_MS);
+}
+
+REGISTER_TEST(R0003, VIRTIO_PCI_DEVICE_MEM, test_mem_unplug,
+              "Unplug a single memory block",
+              VIRTIO_SPEC_V1_2, "5.14.6.2");
