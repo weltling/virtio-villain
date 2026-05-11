35638a26d78bda47121b944ffb71fbc273c16db2
Anatol Belski <anbelski@linux.microsoft.com>
2026-05-11 15:04:08 +0200

    tests: N0103 hash config with undefined type bits
    Send hash config with undefined upper bits set in
    hash_types. Reserved bits must be ignored or the command
    rejected gracefully. Spec v1.3 section 5.1.6.5.4.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

---
+++ tests/net/n0103_hash_tunnel_unknown_type.c
@@ -0,0 +1,68 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * N0103: net_hash_tunnel_unknown_type
+ *
+ * Send hash config with hash_types containing undefined bits set.
+ * Spec v1.3 5.1.6.5.4: reserved hash type bits must be ignored
+ * or the command rejected gracefully.
+ */
+#include "tests/test.h"
+#include "lib/util.h"
+#include "lib/vring.h"
+#include "lib/virtio_pci.h"
+
+#include <string.h>
+#include <unistd.h>
+
+struct virtio_net_ctrl_hdr {
+    uint8_t class;
+    uint8_t command;
+} __attribute__((packed));
+
+#define VIRTIO_NET_CTRL_MQ             4
+#define VIRTIO_NET_CTRL_MQ_HASH_CONFIG 4
+
+struct virtio_net_hash_config {
+    uint32_t hash_types;
+    uint16_t indirection_table_mask;
+    uint16_t unclassified_queue;
+    uint8_t  hash_key_length;
+    uint8_t  hash_key[40];
+} __attribute__((packed));
+
+static test_result_t test_net_hash_unknown_type(struct virtio_dev *dev,
+                                                struct vring *vr)
+{
+    struct virtio_net_ctrl_hdr *ctrl = vv_alloc_pages(1);
+    struct virtio_net_hash_config *hcfg =
+        (struct virtio_net_hash_config *)((uint8_t *)ctrl + sizeof(*ctrl));
+    uint8_t *status = vv_alloc_pages(1);
+
+    ctrl->class = VIRTIO_NET_CTRL_MQ;
+    ctrl->command = VIRTIO_NET_CTRL_MQ_HASH_CONFIG;
+    hcfg->hash_types = 0xFFFF0000;  /* Undefined upper bits */
+    hcfg->indirection_table_mask = 0x0F;
+    hcfg->unclassified_queue = 0;
+    hcfg->hash_key_length = 40;
+    memset(hcfg->hash_key, 0xAB, 40);
+    *status = 0xFF;
+
+    uint64_t ctrl_phys = vv_virt_to_phys(ctrl);
+    uint64_t status_phys = vv_virt_to_phys(status);
+
+    vring_raw_set_desc(vr, 0, ctrl_phys, sizeof(*ctrl),
+                       VRING_DESC_F_NEXT, 1);
+    vring_raw_set_desc(vr, 1, ctrl_phys + sizeof(*ctrl), sizeof(*hcfg),
+                       VRING_DESC_F_NEXT, 2);
+    vring_raw_set_desc(vr, 2, status_phys, 1,
+                       VRING_DESC_F_WRITE, 0);
+
+    vring_raw_set_avail(vr, 0, 0);
+    vring_raw_set_avail_idx(vr, 1);
+
+    return vv_kick_and_wait(dev, vr, 0, VV_TIMEOUT_MS);
+}
+
+REGISTER_TEST_Q(N0103, VIRTIO_PCI_DEVICE_NET, test_net_hash_unknown_type,
+                "Hash config with undefined hash_types bits",
+                VIRTIO_SPEC_V1_3, "5.1.6.5.4", VV_QUEUE_LAST);
