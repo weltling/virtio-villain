424822bb4a0eae8422bd72bde82c12824d0da0c2
Anatol Belski <anbelski@linux.microsoft.com>
2026-07-16 08:23:59 +0200

    tests: N0156 CTRL RX all unicast enable
    Send CTRL_RX_ALLUNI with data=1 and verify VIRTIO_NET_OK.
    Spec 5.1.6.5.1.
    
    Signed-off-by: Anatol Belski <anbelski@linux.microsoft.com>

---
+++ tests/net/n0156_ctrl_rx_alluni.c
@@ -0,0 +1,57 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/*
+ * N0156: ctrl RX all-unicast enable.
+ *
+ * Spec 5.1.6.5.1: Send CTRL_RX_ALLUNI with data=1 (enable
+ * all-unicast mode). Verify VIRTIO_NET_OK.
+ */
+#include "tests/test.h"
+#include "lib/util.h"
+#include "lib/vring.h"
+#include "lib/virtio_pci.h"
+#include "lib/virtio_spec.h"
+
+#include <string.h>
+#include <unistd.h>
+
+static test_result_t test_net_ctrl_alluni(struct virtio_dev *dev,
+                                          struct vring *vr)
+{
+    volatile struct virtio_pci_common_cfg *cfg = dev->common;
+    cfg->device_feature_select = 0;
+    __sync_synchronize();
+    if (!(cfg->device_feature & (1U << VIRTIO_NET_F_CTRL_RX)))
+        return TEST_SKIP;
+    if (!(cfg->device_feature & (1U << VIRTIO_NET_F_CTRL_VQ)))
+        return TEST_SKIP;
+
+    struct virtio_net_ctrl_hdr *hdr = vv_alloc_pages(1);
+    uint8_t *data = vv_alloc_pages(1);
+    uint8_t *ack = vv_alloc_pages(1);
+
+    hdr->class = VIRTIO_NET_CTRL_RX;
+    hdr->command = VIRTIO_NET_CTRL_RX_ALLUNI;
+    *data = 1;
+    *ack = 0xFF;
+
+    vring_raw_set_desc(vr, 0, vv_virt_to_phys(hdr), sizeof(*hdr),
+                       VRING_DESC_F_NEXT, 1);
+    vring_raw_set_desc(vr, 1, vv_virt_to_phys(data), 1,
+                       VRING_DESC_F_NEXT, 2);
+    vring_raw_set_desc(vr, 2, vv_virt_to_phys(ack), 1,
+                       VRING_DESC_F_WRITE, 0);
+    vring_raw_set_avail(vr, 0, 0);
+    vring_raw_set_avail_idx(vr, 1);
+
+    test_result_t r = vv_kick_and_wait(dev, vr, 0, VV_TIMEOUT_MS);
+    if (r != TEST_PASS) return r;
+    if (*ack != VIRTIO_NET_OK) TFAIL("ack %u", *ack);
+
+    return TEST_PASS;
+}
+
+REGISTER_TEST_Q_REQUIRES(N0156, VIRTIO_PCI_DEVICE_NET, test_net_ctrl_alluni,
+              "CTRL RX all-unicast enable",
+              VIRTIO_SPEC_V1_2, "5.1.6.5.1", VV_QUEUE_LAST,
+              (1ULL << VIRTIO_NET_F_CTRL_RX) |
+              (1ULL << VIRTIO_NET_F_CTRL_VQ), 0);
